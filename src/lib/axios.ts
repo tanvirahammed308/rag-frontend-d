ada740001e990b6b4f7600b52f161defe912ef07
Tanvir <imtanvir.email@gmail.com>
2026-09-01 16:48:43 +0600

    fixed error

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,9 +5,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  
 });
 
 export default api;
